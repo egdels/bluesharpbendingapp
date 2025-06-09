c18507c0308900d2a5b61b10fa229a7a76938d13
NiloCK <colin@mrkennedy.ca>
2025-06-09 14:34:14 -0300

    feat: add an npm package definition
    commit creates package.json and some export barrel files which pull
    utilities of note from their original location.

---
+++ npm/src/index.js
@@ -0,0 +1,5 @@
+// BlueSharp Pitch Detection - Main Entry Point
+export * from './pitch-detectors.js';
+export * from './utils.js';
+export * from './chord-detection.js';
+// Note: audio-worklet requires separate import due to browser-specific APIs
